e19941ef91ee985e05e760ca09ce81350698fc72
tivasic <tiv111@mail.ru>
2020-08-03 00:48:42 +0300

    update

--- src/test_server.py
+++ src/test_server.py
@@ -6,15 +6,14 @@ from schemas import Data
 class TestServer(unittest.TestCase):
 
     def test_current_day(self):
-        self.assertEqual(server.current_day(), "Monday")
+        self.assertEqual(server.current_day(), "Sunday")
 
     def test_current_weekday(self):
-        self.assertEqual(server.current_weekday(Data), "Monday")
+        self.assertEqual(server.current_weekday(Data), "Sunday")
 
     def test_future_date(self):
-        self.assertEqual(server.future_date(0, 0), "Monday")
+        self.assertEqual(server.future_date(0, 0), "Sunday")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
