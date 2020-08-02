e27d4f0f54738afea94ed118a7f35772d30b2514
Tivasic <68960327+Tivasic@users.noreply.github.com>
2020-08-03 02:49:40 +0300

    unittest

--- src/test_server.py
+++ src/test_server.py
@@ -1,6 +1,20 @@
+import unittest
 import server
 from schemas import Data
 
 
-def test_day():
-    assert server.current_day() == "Sunday"
+class TestServer(unittest.TestCase):
+
+    def test_current_day(self):
+        self.assertEqual(server.current_day(), "Monday")
+
+    def test_current_weekday(self):
+        self.assertEqual(server.current_weekday(Data), "Monday")
+
+    def test_future_date(self):
+        self.assertEqual(server.future_date(0, 0), "Monday")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
