fdc484df6858a422bb2971c224882673e8424ec1
tivasic <tiv111@mail.ru>
2020-08-03 19:13:27 +0300

    tests with pytest

--- src/test_server.py
+++ src/test_server.py
@@ -1,19 +1,18 @@
-import unittest
 import server
 from schemas import Data
 
 
-class TestServer(unittest.TestCase):
-
-    def test_current_day(self):
-        self.assertEqual(server.current_day(), "Sunday")
-
-    def test_current_weekday(self):
-        self.assertEqual(server.current_weekday(Data), "Sunday")
-
-    def test_future_date(self):
-        self.assertEqual(server.future_date(0, 0), "Sunday")
+def test_current_day():
+    assert server.current_day() == "Monday"
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_current_weekday():
+    assert server.current_weekday(Data) == "Monday"
+
+
+def test_future_date():
+    assert server.future_date(0, 0) == "Monday"
+
+
+
+
