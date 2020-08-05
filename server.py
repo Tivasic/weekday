eb39a40c548190bb2d435e81a81d33419d3bdd0b
tivasic <tiv111@mail.ru>
2020-08-06 00:00:51 +0300

    Update

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import datetime
+
 from fastapi import FastAPI
+
 from schemas import Calc, Data
 from dictionary import weekdays
 
@@ -7,31 +9,33 @@ from dictionary import weekdays
 app = FastAPI()
 
 
-# 1.GET-запрос, которые возвращает текущий день недели на сервере
-
+# 1.GET-запрос, которые возвращает текущий день недели на сервере.
 
 @app.get('/')
 def current_day():
+    # Получает текущий день недели.
     day = weekdays[datetime.datetime.today().isoweekday()]
     return day
 
 
-# 2. POST-запрос, в теле которого приходит дата и возвращается день недели.
-
+# 2. POST-запрос, в теле которого приходит дата
+# и возвращается день недели.
 
 @app.post('/')
 def current_weekday(item: Data):
+    # Отправляет на сервер текущую дату и
+    # возвращает день недели.
     item.weekday = weekdays[datetime.datetime.today().isoweekday()]
     return item.weekday
 
 
-# 3. Запрос, который скажет, какой день недели будет через указанное время
-#    (N дней, M часов, ...) от текущей даты
-
+# 3. Запрос, который скажет, какой день недели будет через указанное
+# время (N дней, M часов, ...) от текущей даты.
 
 @app.post('/future_date')
 def future_date(numberdays, numberhrs):
-
+    # Отправляет на сервер указанные дни и часы
+    # и возвращает день недели через указанное время.
     Calc.futuredate = datetime.datetime.today().now()
     numberdays = int(numberdays)
     numberhrs = int(numberhrs)
--- src/test_server.py
+++ src/test_server.py
@@ -1,4 +1,5 @@
 import server
+
 from schemas import Data
 
 
